94f1c99f1f842024dec88feecb5581dd4efe75f5
Konstantin Litvinenko <to.darkangel@gmail.com>
2021-11-09 11:11:57 +0200

    added .bash_aliases to filetypes

--- nvim/.config/nvim/after/plugin/filetype.lua
+++ nvim/.config/nvim/after/plugin/filetype.lua
@@ -2,6 +2,7 @@ require('filetype').setup({
     overrides = {
         literal = {
             ['.bashrc.local'] = 'sh',
+            ['.bash_aliases'] = 'sh',
             ['.profile.local'] = 'sh',
         }
     }
