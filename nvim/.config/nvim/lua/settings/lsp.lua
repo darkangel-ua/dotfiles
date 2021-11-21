951462e2e1bfec7d46698c3eda9923e2249d979f
Konstantin Litvinenko <to.darkangel@gmail.com>
2021-11-21 13:05:35 +0200

    made lsp caps initialization as recomened in nvim-cmp docs

--- nvim/.config/nvim/lua/settings/lsp.lua
+++ nvim/.config/nvim/lua/settings/lsp.lua
@@ -1,7 +1,6 @@
 local lsp_status = require('lsp-status')
 local lspconfig = require('lspconfig')
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities.textDocument.completion.completionItem.snippetSupport = true
+local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 lsp_status.config({
     show_filename = false,
