e3305be5ae779b920d27d1e4ce9283e3591ac4cc
Konstantin Litvinenko <to.darkangel@gmail.com>
2022-01-01 11:00:19 +0200

    Added gtest helpers because using upper case is annoying as hell

--- nvim/.config/nvim/lua/settings/snippets.lua
+++ nvim/.config/nvim/lua/settings/snippets.lua
@@ -33,6 +33,17 @@ public:
 ls.snippets.cpp = {
     ls.parser.parse_snippet({trig = "main"}, cpp_main),
     ls.parser.parse_snippet({trig = "class"}, cpp_class),
+    -- gtest
+    ls.parser.parse_snippet({trig = "test"}, "GTEST_TEST(${1:test-suit-name}, ${2:test-name}) {\n\t$0\n}"),
+    ls.parser.parse_snippet({trig = "testf"}, "TEST_F(${1:test-fixture}, ${2:test-name}) {\n\t$0\n}"),
+    ls.parser.parse_snippet({trig = "etrue"}, "EXPECT_TRUE(${1:condition});$0"),
+    ls.parser.parse_snippet({trig = "efalse"}, "EXPECT_FALSE(${1:condition});$0"),
+    ls.parser.parse_snippet({trig = "eeq"}, "EXPECT_EQ(${1:expected}, ${2:actual});$0"),
+    ls.parser.parse_snippet({trig = "ene"}, "EXPECT_NE(${1:val1}, ${2:val2});$0"),
+    ls.parser.parse_snippet({trig = "enothrow"}, "EXPECT_NO_THROW(${1:expression});$0"),
+    ls.parser.parse_snippet({trig = "eanythrow"}, "EXPECT_ANY_THROW(${1:expression});$0"),
+    ls.parser.parse_snippet({trig = "ethrow"}, "EXPECT_THROW(${1:expression}, ${2:expected-exception});$0"),
+    ls.parser.parse_snippet({trig = "ex"}, "EXPECT"), -- this helps discover other macroses
 }
 
 -- load local snippets configuration
