7e3b0a16c1579cb316817629c3fc1a4fe34e2e5e
Konstantin Litvinenko <to.darkangel@gmail.com>
2022-04-17 20:37:25 +0300

    fix snippets after update

--- nvim/.config/nvim/lua/settings/snippets.lua
+++ nvim/.config/nvim/lua/settings/snippets.lua
@@ -30,7 +30,7 @@ public:
 };
 ]]
 
-ls.snippets.cpp = {
+ls.add_snippets("cpp", {
     ls.parser.parse_snippet({trig = "main"}, cpp_main),
     ls.parser.parse_snippet({trig = "class"}, cpp_class),
     -- gtest
@@ -51,7 +51,7 @@ ls.snippets.cpp = {
     ls.parser.parse_snippet({trig = "anothrow"}, "ASSERT_NO_THROW(${1:expression});$0"),
     ls.parser.parse_snippet({trig = "aanythrow"}, "ASSERT_ANY_THROW(${1:expression});$0"),
     ls.parser.parse_snippet({trig = "athrow"}, "ASSERT_THROW(${1:expression}, ${2:expected-exception});$0"),
-}
+})
 
 -- load local snippets configuration
 pcall(require, "settings.snippets_local")
