a9641218e3174f9cf953572f10c49a2c561ca295
Konstantin Litvinenko <to.darkangel@gmail.com>
2022-05-29 14:00:58 +0300

    Discovered 'top' command!

--- nvim/.config/nvim/lua/settings/plugins/diffview.lua
+++ nvim/.config/nvim/lua/settings/plugins/diffview.lua
@@ -1,37 +1,17 @@
 local cb = require'diffview.config'.diffview_callback
 local M = {}
 
-local commit_requested = false
-
-function M.commit()
-    commit_requested = true
-    vim.cmd('Git commit')
-end
-
-function M.buf_enter()
-    if not commit_requested then return end
-    commit_requested = false
-    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-w>K", true, false, true), 'n', true)
-end
-
 require('diffview').setup {
   key_bindings = {
     view = {
-      ["<leader>x"] = ':DiffviewClose<cr>',
+      ["<leader>x"] = ':silent DiffviewClose<cr>',
     },
     file_panel = {
-      ["<leader>x"] = ':DiffviewClose<cr>',
+      ["<leader>x"] = ':silent DiffviewClose<cr>',
       ["go"] = cb('focus_entry'),
-      ["cc"] = ':lua require("settings.plugins.diffview").commit()<cr>',
+      ["cc"] = ':silent top Git commit<cr>',
     }
   }
 }
-
-vim.cmd[[
-augroup my_diffview
-    autocmd!
-    autocmd BufEnter * lua require('settings.plugins.diffview').buf_enter()
-augroup END
-]]
 
 return M
