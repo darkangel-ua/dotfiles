6284f131b09ade66c6aff0b4827b908b13a34fe3
Konstantin Litvinenko <to.darkangel@gmail.com>
2022-07-23 10:19:39 +0300

    Make sign column the same background as buffer background. Some formatting

--- nvim/.config/nvim/lua/settings/plugins/gruvbox.lua
+++ nvim/.config/nvim/lua/settings/plugins/gruvbox.lua
@@ -5,20 +5,30 @@ function M.setup()
     require("gruvbox").setup({
         contrast = "hard",
         overrides = {
+            -- make sign column same as background
+            SignColumn = { bg = colors.dark0_hard },
+            GruvboxRedSign = { bg = colors.dark0_hard },
+            GruvboxYellowSign = { bg = colors.dark0_hard },
+            GruvboxGreenSign = { bg = colors.dark0_hard },
+            GruvboxAquaSign = { bg = colors.dark0_hard },
+
             BufferLineFill = { bg = colors.dark0_hard },
             BufferLineSeparator = { fg = colors.dark0 },
             BufferLineIndicatorSelected = { fg = colors.dark0_hard },
+
             LuasnipInsertNodePassive = { fg = colors.bright_blue, bg = colors.dark1, italic = true },
             LuasnipChoiceNodePassive = { fg = colors.bright_blue, bg = colors.dark1, italic = true },
             LuasnipExitNodePassive = { fg = colors.bright_red, bg = colors.dark1, italic = true },
+
             HopNextKey = { link = "GruvboxOrangeBold" },
             HopNextKey1 = { link = "GruvboxOrangeBold" },
             HopNextKey2 = { link = "GruvboxOrange" },
+
             Pmenu = { bg = colors.dark1 },
         },
     })
 
-    vim.cmd [[ colorscheme gruvbox ]]
+    vim.cmd('colorscheme gruvbox')
 end
 
 return M
