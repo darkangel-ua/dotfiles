193267d0ca3cfd1aa8c45640f6682a46b29e2559
Konstantin Litvinenko <to.darkangel@gmail.com>
2022-07-24 18:14:16 +0300

    Don't autosave session if it wasn't opened

--- nvim/.config/nvim/lua/settings/plugins/neovim-session-manager.lua
+++ nvim/.config/nvim/lua/settings/plugins/neovim-session-manager.lua
@@ -3,6 +3,7 @@ local M = {}
 function M.setup()
     require('session_manager').setup({
         autoload_mode = require('session_manager.config').AutoloadMode.Disabled,
+        autosave_only_in_session = true,
     })
 end
 
