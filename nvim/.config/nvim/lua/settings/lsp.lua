f470617bb7a81ada614535d6f32edefdc33d5fed
Konstantin Litvinenko <to.darkangel@gmail.com>
2022-10-16 09:57:34 +0300

    Fix deprecation warning

--- nvim/.config/nvim/lua/settings/lsp.lua
+++ nvim/.config/nvim/lua/settings/lsp.lua
@@ -1,12 +1,11 @@
 local lspconfig = require('lspconfig')
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 lspconfig.clangd.setup({
   init_options = {
     clangdFileStatus = true
   },
   on_attach = require('settings.plugins.nvim-navic').attach,
-  capabilities = capabilities,
+  capabilities = require('cmp_nvim_lsp').default_capabilities(),
 })
 
 -- make signs same as in trouble.nvim
