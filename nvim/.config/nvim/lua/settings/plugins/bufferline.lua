253017331798c73cd451afc63d6be3cb0bf1335e
Konstantin Litvinenko <to.darkangel@gmail.com>
2022-10-16 13:14:48 +0300

    Better control over buffer line highlights because doing overrides in gruvbox config doesn't give desirable results

--- nvim/.config/nvim/lua/settings/plugins/bufferline.lua
+++ nvim/.config/nvim/lua/settings/plugins/bufferline.lua
@@ -1,6 +1,7 @@
 local M = {}
 
 function M.setup()
+    local colors = require('gruvbox.palette')
     require("bufferline").setup({
         options = {
             numbers = function(opts)
@@ -9,6 +10,28 @@ function M.setup()
             show_buffer_close_icons = false,
             show_close_icon = false,
         },
+        highlights = {
+            fill = {
+                bg = colors.dark0_hard,
+            },
+            background = {
+                fg = colors.dark4,
+                bg = colors.dark0_hard,
+            },
+            numbers = {
+                bg = colors.dark0_hard,
+            },
+            modified = {
+                bg = colors.dark0_hard,
+            },
+            separator = {
+                fg = colors.dark0,
+                bg = colors.dark0_hard,
+            },
+            indicator_selected = {
+                fg = colors.dark0_hard,
+            },
+        }
     })
 end
 
--- nvim/.config/nvim/lua/settings/plugins/gruvbox.lua
+++ nvim/.config/nvim/lua/settings/plugins/gruvbox.lua
@@ -17,13 +17,6 @@ function M.setup()
             DashboardShortcut = { link = 'GruvboxRed' },
             DashboardFooter = { link = 'GruvboxYellow' },
 
-            BufferLineFill = { bg = colors.dark0_hard },
-            BufferLineBackground = { fg = colors.dark4, bg = colors.dark0_hard },
-            BufferLineNumbers = { fg = colors.dark4, bg = colors.dark0_hard },
-            BufferLineModified = { fg = colors.bright_green, bg = colors.dark0_hard },
-            BufferLineSeparator = { fg = colors.dark0 },
-            BufferLineIndicatorSelected = { fg = colors.dark0_hard },
-
             LuasnipInsertNodePassive = { fg = colors.bright_blue, bg = colors.dark1, italic = true },
             LuasnipChoiceNodePassive = { fg = colors.bright_blue, bg = colors.dark1, italic = true },
             LuasnipExitNodePassive = { fg = colors.bright_red, bg = colors.dark1, italic = true },
