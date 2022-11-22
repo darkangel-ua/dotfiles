b0dc6afba48591608a75bc98a569efb8fcd0b926
Konstantin Litvinenko <to.darkangel@gmail.com>
2022-11-22 23:13:40 +0200

    Use lua functions where possible

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -6,7 +6,7 @@ local keymap = function(mode, lhs, rhs, opts)
 end
 
 -- General movement and editing mappings
-keymap('n', '<leader>x', function() require('settings.plugins.ide').bufdelete() end, { desc ='' })
+keymap('n', '<leader>x', require('settings.plugins.ide').bufdelete, { desc ='' })
 -- even more stupid way to close tab
 keymap('n', '<leader>X', ":tabc<cr>", { desc ='' })
 -- Set kj/jk to be escape in insert mode
@@ -52,19 +52,19 @@ keymap('i', '<C-P>', "<C-o>:Telescope neoclip plus<cr>", { desc ='' })
 keymap('v', '<C-P>', ":Telescope neoclip plus<cr>", { desc ='' })
 
 -- Telescope
-keymap('n', '<leader>ff', "<cmd>lua require('telescope.builtin').find_files()<cr>", { desc ='' })
-keymap('n', '<leader>fF', "<cmd>lua require('settings.plugins.telescope').find_files_buffer()<cr>", { desc ='' })
-keymap('n', '<leader>fd', "<cmd>lua require('settings.plugins.telescope').dotfiles()<cr>", { desc ='' })
-keymap('n', '<leader>fD', "<cmd>lua require('settings.plugins.telescope').dotfiles_local()<cr>", { desc ='' })
-keymap('n', '<leader>fe', "<cmd>lua require 'telescope'.extensions.file_browser.file_browser()<cr>", { desc ='' })
-keymap('n', '<leader>fE', "<cmd>lua require('settings.plugins.telescope').file_browser_buffer()<cr>", { desc ='' })
-keymap('n', '<leader>fg', "<cmd>lua require('telescope.builtin').git_files()<cr>", { desc ='' })
-keymap('n', '<leader>fo', "<cmd>lua require('telescope.builtin').oldfiles()<cr>", { desc ='' })
-keymap('n', '<leader>fb', "<cmd>lua require('telescope.builtin').buffers()<cr>", { desc ='' })
-keymap('n', '<leader>fh', "<cmd>lua require('telescope.builtin').help_tags()<cr>", { desc ='' })
-keymap('n', '<leader>fs', "<cmd>lua require('telescope.builtin').live_grep({additional_args = function() return {'--hidden', '--iglob', '!.git/'} end})<cr>", { desc ='' })
-keymap('n', '<leader>fw', "<cmd>lua require('telescope.builtin').grep_string()<cr>", { desc ='' })
-keymap('n', '<leader>fW', "<cmd>lua require('settings.plugins.telescope').grep_string_in_buffer()<cr>", { desc ='' })
+keymap('n', '<leader>ff', require('telescope.builtin').find_files, { desc ='' })
+keymap('n', '<leader>fF', require('settings.plugins.telescope').find_files_buffer, { desc ='' })
+keymap('n', '<leader>fd', require('settings.plugins.telescope').dotfiles, { desc ='' })
+keymap('n', '<leader>fD', require('settings.plugins.telescope').dotfiles_local, { desc ='' })
+keymap('n', '<leader>fe', require 'telescope'.extensions.file_browser.file_browser, { desc ='' })
+keymap('n', '<leader>fE', require('settings.plugins.telescope').file_browser_buffer, { desc ='' })
+keymap('n', '<leader>fg', require('telescope.builtin').git_files, { desc ='' })
+keymap('n', '<leader>fo', require('telescope.builtin').oldfiles, { desc ='' })
+keymap('n', '<leader>fb', require('telescope.builtin').buffers, { desc ='' })
+keymap('n', '<leader>fh', require('telescope.builtin').help_tags, { desc ='' })
+keymap('n', '<leader>fs', function() require('telescope.builtin').live_grep({additional_args = function() return {'--hidden', '--iglob', '!.git/'} end}) end, { desc ='' })
+keymap('n', '<leader>fw', require('telescope.builtin').grep_string, { desc ='' })
+keymap('n', '<leader>fW', require('settings.plugins.telescope').grep_string_in_buffer, { desc ='' })
 keymap('n', '<leader>p', "<cmd>Telescope resume<cr>", { desc ='' })
 keymap('n', 'S', ":Telescope spell_suggest<cr>", { desc ='' })
 keymap('n', '<leader>fm', "<cmd>Telescope man_pages<cr>", { desc ='' })
@@ -73,15 +73,15 @@ keymap('n', '<leader>fm', "<cmd>Telescope man_pages<cr>", { desc ='' })
 keymap('n', '<leader>fp', ':Telescope projects<cr>', { desc = 'Find a project' })
 
 -- buffers
-keymap('n', '<leader>1', "<Cmd>lua require('bufferline').go_to(1, true)<cr>", { desc ='' })
-keymap('n', '<leader>2', "<Cmd>lua require('bufferline').go_to(2, true)<cr>", { desc ='' })
-keymap('n', '<leader>3', "<Cmd>lua require('bufferline').go_to(3, true)<cr>", { desc ='' })
-keymap('n', '<leader>4', "<Cmd>lua require('bufferline').go_to(4, true)<cr>", { desc ='' })
-keymap('n', '<leader>5', "<Cmd>lua require('bufferline').go_to(5, true)<cr>", { desc ='' })
-keymap('n', '<leader>6', "<Cmd>lua require('bufferline').go_to(6, true)<cr>", { desc ='' })
-keymap('n', '<leader>7', "<Cmd>lua require('bufferline').go_to(7, true)<cr>", { desc ='' })
-keymap('n', '<leader>8', "<Cmd>lua require('bufferline').go_to(8, true)<cr>", { desc ='' })
-keymap('n', '<leader>9', "<Cmd>lua require('bufferline').go_to(9, true)<cr>", { desc ='' })
+keymap('n', '<leader>1', function() require('bufferline').go_to(1, true) end, { desc ='' })
+keymap('n', '<leader>2', function() require('bufferline').go_to(2, true) end, { desc ='' })
+keymap('n', '<leader>3', function() require('bufferline').go_to(3, true) end, { desc ='' })
+keymap('n', '<leader>4', function() require('bufferline').go_to(4, true) end, { desc ='' })
+keymap('n', '<leader>5', function() require('bufferline').go_to(5, true) end, { desc ='' })
+keymap('n', '<leader>6', function() require('bufferline').go_to(6, true) end, { desc ='' })
+keymap('n', '<leader>7', function() require('bufferline').go_to(7, true) end, { desc ='' })
+keymap('n', '<leader>8', function() require('bufferline').go_to(8, true) end, { desc ='' })
+keymap('n', '<leader>9', function() require('bufferline').go_to(9, true) end, { desc ='' })
 keymap('n', '<leader>0', ":BufferLineTogglePin<cr> <bar> :lua require('bufferline').sort_by('none')<cr>", { desc ='' })
 -- this is something similar to Ctrl-Tab in most IDEs - to switch between buffers
 keymap('n', '<leader>j', "<cmd>lua require('settings.plugins.telescope').quick_buffers()<cr>", { desc ='' })
@@ -95,14 +95,14 @@ keymap('n', '<leader>gb', ":Git blame<cr>", { desc ='' })
 keymap('n', '<leader>gl', ":Telescope git_commits<cr>", { desc ='' })
 
 -- lsp
-keymap('n', 'gd', ":lua require('settings.plugins.telescope').lsp_definitions()<cr>", { desc ='' })
+keymap('n', 'gd', require('settings.plugins.telescope').lsp_definitions, { desc ='' })
 keymap('n', 'gD', ":Telescope lsp_type_definitions<cr>", { desc ='' })
 keymap('n', 'gi', ":Telescope lsp_implementations<cr>", { desc ='' })
 keymap('v', 'gi', ":TSCppDefineClassFunc<cr>", { desc ='' })
-keymap('n', 'gr', "<cmd>lua require('settings.plugins.telescope').lsp_references()<cr>", { desc ='' })
+keymap('n', 'gr', require('settings.plugins.telescope').lsp_references, { desc ='' })
 keymap('n', 'gh', ":ClangdSwitchSourceHeader<cr>", { desc ='' })
 keymap('n', 'ga', ":CodeActionMenu<cr>", { desc ='' })
-keymap('n', 'K', ":lua vim.lsp.buf.hover()<CR>", { desc ='' })
+keymap('n', 'K', vim.lsp.buf.hover, { desc ='' })
 keymap('n', '<leader>rn', ":lua vim.lsp.buf.rename()<CR>", { desc ='' })
 keymap('n', '<leader>o', function() require('settings.plugins.telescope').lsp_document_symbols() end, { desc ='' })
 keymap('n', 'gs', function() require('settings.plugins.telescope').lsp_dynamic_workspace_symbols() end, { desc ='' })
@@ -115,28 +115,28 @@ keymap('n', '<leader>;', ":TroubleToggle quickfix<cr>:CMakeClose<cr>", { desc ='
 
 -- building
 keymap('n', '<leader>mm', ":TroubleClose<cr> <bar> :CMakeBuild<cr>", { desc ='' })
-keymap('n', '<leader>mo', "<cmd>lua require('settings.plugins.ide').cmake_open()<cr>", { desc ='' })
+keymap('n', '<leader>mo', require('settings.plugins.ide').cmake_open, { desc ='' })
 keymap('n', '<leader>mc', ":CMakeClose<cr>", { desc ='' })
-keymap('n', '<leader>mt', ":lua require('settings.plugins.cmake').select_build_targets()<cr>", { desc ='' })
+keymap('n', '<leader>mt', require('settings.plugins.cmake').select_build_targets, { desc ='' })
 
 -- debugging
-keymap('n', '<F5>', ":lua require('dap').continue()<cr>", { desc ='' })
-keymap('n', '<F8>', ":lua require('dap').step_over()<cr>", { desc ='' })
-keymap('n', '<F9>', ":lua require('dap').step_into()<cr>", { desc ='' })
-keymap('n', '<F21>', ":lua require('dap').step_out()<cr>", { desc ='' }) -- F21 is actually Shift-F9
-keymap('n', '<F2>', ":lua require('dap').toggle_breakpoint()<cr>", { desc ='' })
-keymap('n', '<F22>', ":lua require('dap').terminate()<cr>", { desc ='' }) -- F22 is actually Shift-F10
+keymap('n', '<F5>', require('dap').continue, { desc ='' })
+keymap('n', '<F8>', require('dap').step_over, { desc ='' })
+keymap('n', '<F9>', require('dap').step_into, { desc ='' })
+keymap('n', '<F21>', require('dap').step_out, { desc ='' }) -- F21 is actually Shift-F9
+keymap('n', '<F2>', require('dap').toggle_breakpoint, { desc ='' })
+keymap('n', '<F22>', require('dap').terminate, { desc ='' }) -- F22 is actually Shift-F10
 
 -- reloading config/files
-keymap('n', '<leader>rc', ":lua require('settings.reload').reload_all()<cr>", { desc ='' })
-keymap('n', '<leader>rf', ":lua require('settings.reload').source_current_file()<cr>", { desc ='' })
+keymap('n', '<leader>rc', require('settings.reload').reload_all, { desc ='' })
+keymap('n', '<leader>rf', require('settings.reload').source_current_file, { desc ='' })
 
 -- ide like behaviour to close support windows by esc leaving only main window
-keymap('n', '<esc>', "<cmd>lua require('settings.plugins.ide').on_esc_pressed()<cr>", { desc ='' })
+keymap('n', '<esc>', require('settings.plugins.ide').on_esc_pressed, { desc ='' })
 
 -- scratch terminal
-keymap('n', '<C-h>', "<cmd>lua require('FTerm').toggle()<cr>", { desc ='' })
-keymap('t', '<C-h>', "<cmd>lua require('FTerm').toggle()<cr>", { desc ='' })
+keymap('n', '<C-h>', require('FTerm').toggle, { desc ='' })
+keymap('t', '<C-h>', require('FTerm').toggle, { desc ='' })
 
 -- vim tmux integration
 keymap('n', '<A-h>', ':TmuxNavigateLeft<cr>', { desc ='' })
@@ -157,8 +157,8 @@ function M.dashboard()
     keymap('n', 'O', ':SessionManager load_session<cr>', { buffer = true })
     keymap('n', 'S', ':SessionManager load_last_session<cr>', { buffer = true })
     keymap('n', 'C', ':SessionManager load_current_dir_session<cr>', { buffer = true })
-    keymap('n', 'o', ":lua require('telescope.builtin').oldfiles()<cr>", { buffer = true })
-    keymap('n', 's', ":lua require('settings.plugins.telescope').dotfiles()<cr>", { buffer = true })
+    keymap('n', 'o', function() require('telescope.builtin').oldfiles() end, { buffer = true })
+    keymap('n', 's', function() require('settings.plugins.telescope').dotfiles() end, { buffer = true })
     keymap('n', 'q', ':q<cr>', { buffer = true })
 end
 
