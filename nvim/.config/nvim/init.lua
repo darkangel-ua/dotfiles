e6f54ba4eeb38f1853a17f4f5997fb80ca549f72
Konstantin Litvinenko <to.darkangel@gmail.com>
2022-12-03 20:15:04 +0200

    Change plugins and keymaps order - anyways packer can't do lazy key mapping in the way I want

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -6,11 +6,11 @@ vim.o.termguicolors = true
 local r, m = pcall(require, 'impatient')
 -- if r then m.enable_profile() end -- use :LuaCacheProfile to see profiling data
 
+require('plugins')
 -- keymaps should be infront of plugins so packer will be able to use keymap lazy loading
--- but it's actually doesn't work - packer will not save/restore keys. It expects that after
+-- but actually it doesn't work - packer will not save/restore keys. It expects that after
 -- plugin will be loaded it will re-define mappings which is not true in most of the cases
 require('keymaps')
-require('plugins')
 require('packer_compiled')
 require('settings')
 
