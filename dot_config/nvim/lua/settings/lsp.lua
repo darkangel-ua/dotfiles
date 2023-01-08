0852644a872a78b2afc9491751a668358de839c2
Konstantin Litvinenko <to.darkangel@gmail.com>
2023-01-08 15:01:01 +0200

    Configure cmake LSP and fix CursonHold logic

--- dot_config/nvim/lua/settings/lsp.lua
+++ dot_config/nvim/lua/settings/lsp.lua
@@ -1,11 +1,35 @@
-local lspconfig = require('lspconfig')
+local
+function cursor_hold_actions(client, bufnr)
+    local lightbulb = require('nvim-lightbulb')
+
+    local cursor_hold_acg = vim.api.nvim_create_augroup('cursor_hold', { clear = true })
+    vim.api.nvim_create_autocmd( {'CursorHold', 'CursorHoldI' }, {
+        buffer = bufnr,
+        group = cursor_hold_acg,
+        callback = function()
+            vim.lsp.buf.document_highlight()
+            lightbulb.update_lightbulb()
+        end,
+    })
+    vim.api.nvim_create_autocmd( 'CursorMoved', {
+        buffer = bufnr,
+        group = cursor_hold_acg,
+        callback = vim.lsp.buf.clear_references,
+    })
+end
+
+local
+function on_lsp_attach(client, bufnr)
+    require('nvim-navic').attach(client, bufnr)
+    cursor_hold_actions(client, bufnr)
+end
 
 require('clangd_extensions').setup {
   server = {
       init_options = {
         clangdFileStatus = true
       },
-      on_attach = require('nvim-navic').attach,
+      on_attach = on_lsp_attach,
       capabilities = require('cmp_nvim_lsp').default_capabilities(),
       extensions = {
           autoSetHints = false,
@@ -13,15 +37,16 @@ require('clangd_extensions').setup {
   },
 }
 
+local lspconfig = require('lspconfig')
+lspconfig.cmake.setup {
+    init_options = {
+        buildDirectory = '.build/Debug',
+    },
+}
+
 -- make signs same as in trouble.nvim
 vim.cmd [[
     sign define DiagnosticSignError text= texthl=DiagnosticSignError linehl= numhl=
     sign define DiagnosticSignWarning text= texthl=DiagnosticSignWarning linehl= numhl=
 ]]
-
-vim.api.nvim_command [[autocmd CursorHold  * lua vim.lsp.buf.document_highlight()]]
-vim.api.nvim_command [[autocmd CursorHoldI * lua vim.lsp.buf.document_highlight()]]
-vim.api.nvim_command [[autocmd CursorMoved * lua vim.lsp.buf.clear_references()]]
-
-vim.cmd [[ autocmd CursorHold,CursorHoldI * lua require'nvim-lightbulb'.update_lightbulb() ]]
 
