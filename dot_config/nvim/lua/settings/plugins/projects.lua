77d31f92c5132f9edf46486877cade4c13b6620b
Konstantin Litvinenko <to.darkangel@gmail.com>
2023-01-22 12:42:30 +0200

    In complex cases when I use multiple projects in the same nvim instance it works not well because LSP detection doesn't understand multiple project roots. So moving to pattern first

--- dot_config/nvim/lua/settings/plugins/projects.lua
+++ dot_config/nvim/lua/settings/plugins/projects.lua
@@ -2,6 +2,7 @@ local M = {}
 
 function M.setup() 
     require('project_nvim').setup({
+        detection_methods = { "pattern", "lsp" },
         patterns = { '.git', 'compile_commands.json' }
     })
 end
