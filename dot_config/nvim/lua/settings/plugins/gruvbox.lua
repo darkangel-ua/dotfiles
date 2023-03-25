aab212ead9cd45f540d9e7462ebc53fb2afdc777
Konstantin Litvinenko <to.darkangel@gmail.com>
2023-03-25 21:33:30 +0200

    I don't like new diff colors - they are too saturated, can't see the difference. Revert back to previous variant

--- dot_config/nvim/lua/settings/plugins/gruvbox.lua
+++ dot_config/nvim/lua/settings/plugins/gruvbox.lua
@@ -18,6 +18,11 @@ function M.setup()
             DashboardFooter = { link = 'GruvboxYellow' },
             DashboardIcon = { link = 'GruvboxOrange' },
 
+            DiffDelete = { fg = colors.red, bg = colors.bg0, reverse = true },
+            DiffAdd = { fg = colors.green, bg = colors.bg0, reverse = true },
+            DiffChange = { fg = colors.aqua, bg = colors.bg0, reverse = true },
+            DiffText = { fg = colors.yellow, bg = colors.bg0, reverse = true },
+
             LuasnipInsertNodePassive = { fg = colors.blue, bg = colors.bg1, italic = true },
             LuasnipChoiceNodePassive = { fg = colors.blue, bg = colors.bg1, italic = true },
             LuasnipExitNodePassive = { fg = colors.red, bg = colors.bg1, italic = true },
