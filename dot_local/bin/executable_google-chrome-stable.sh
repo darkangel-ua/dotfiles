eba47847829ae32c7065c67c05cddb4913335413
Konstantin Litvinenko <to.darkangel@gmail.com>
2023-03-25 18:53:28 +0200

    Update google chrome flags

--- dot_local/bin/executable_google-chrome-stable.sh
+++ dot_local/bin/executable_google-chrome-stable.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/bash
 
-# have to disable Vulkan otherwise gaming become disaster
-google-chrome-stable --enable-features=VaapiVideoEncoder,VaapiVideoDecoder,CanvasOopRasterization,Vulkan \
-                     --enable-zero-copy \
-                     --use-gl=desktop \
-                     --ignore-gpu-blocklist \
-                     --enable-gpu-rasterization \
-                     --enable-oop-rasterization \
-                     --canvas-oop-rasterization \
-                     --enable-raw-draw \
-                     --enable-drdc \
-                     --enable-smooth-scrolling \
-                     --enable-vulkan \
-                     "$@"
+google-chrome-stable \
+    --enable-features=VaapiVideoEncoder,VaapiVideoDecoder,CanvasOopRasterization,Vulkan \
+    --enable-zero-copy \
+    --ignore-gpu-blocklist \
+    --enable-gpu-rasterization \
+    --enable-oop-rasterization \
+    --canvas-oop-rasterization \
+    --enable-raw-draw \
+    --enable-drdc \
+    --enable-smooth-scrolling \
+    --enable-vulkan \
+    "$@"
